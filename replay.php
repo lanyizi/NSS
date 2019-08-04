d386e470d8d5ee8b5228a2272405edc59e037255
lanyi <lanyi@ra3.moe>
2019-08-05 01:18:59 +0200

    移除录像解析测试

--- replay.php
+++ replay.php
@@ -92,19 +92,4 @@ class RA3Replay {
     }
 }
 
-
-
-$testReplay = $_GET['replay'];
-if(!empty($testReplay)) {
-    header('Content-Type: application/json;charset=utf-8');
-
-    echo json_encode(RA3Replay::parseRA3Replay(file_get_contents('23333333333333333333333jcc.RA3Replay')));
-    echo '\r\n';
-    echo json_encode(RA3Replay::parseRA3Replay(file_get_contents('233333333333333333333333jc.RA3Replay')));
-    echo '\r\n';
-    echo json_encode(RA3Replay::parseRA3Replay(file_get_contents('2333333333333333333333333.RA3Replay')));
-    echo '\r\n';
-    echo json_encode(RA3Replay::parseRA3Replay(file_get_contents('jujuka23333333333333333333.RA3Replay')));
-}
-
 ?>
