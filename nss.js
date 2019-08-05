a8cf4311dbe04fc6d30828eb3c95f6d23db916ba
RC <richcfno1@users.noreply.github.com>
2019-08-05 12:58:41 -0700

    Test

--- nss.js
+++ nss.js
@@ -1,38 +1,66 @@
 function login() {
-    var transdata = { 'username': username, 'password': password };
+    var transdata = {
+        'username': document.getElementById("login_username").value,
+        'password': document.getElementById("login_password").value
+    };
 
     //temp is a temporary value of feedback
-    var temp = fetch("/nss.php?do=login", {
+    fetch("nss.php?do=login", {
             method: 'post',
             body: JSON.stringify(transdata),
-            headers: { 'Content-Type': 'application/json' }
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
-        .then(res => res.json());
+        .then(res => {
+            return res.json();
+        })
+        .then(feedback => {
+            if (feedback.token == '0') {
+                alert("Wrong username or password")
+                window.location.href = "login_page.html";
+                return "Login Fail";
+            }
+            alert("Login Success")
+                //Get the time and set for 2 hours access
+            var date = new Date();
+            date.setTime(date.getTime() + (2 * 60 * 60 * 1000));
 
-    //If not pass
-    if (temp = '0') {
-        alert("Wrong username or password")
-            //window.location.href = "login_page.html";
-        return "Login Fail";
-    }
+            //Get token
 
-    //Get the time and set for 2 hours access
-    var date = new Date();
-    date.setTime(date.getTime() + (2 * 60 * 60 * 1000));
+            var token = feedback.token;
 
-    //Get token
-    var feedback = JSON.parse(temp);
-    var token = feedback[Object.keys(feedback)[1]];
+            //Set cookie
+            document.cookie = "token=" + token + "; expires=" + date.toGMTString();
 
-    //Set cookie
-    document.cookie = "Token=" + token + "; expires=" + date.toGMTString();
-
-    return "Login Success";
+            //Back to index
+            window.location.href = "index.html";
+            return "Login Success";
+        });
 }
 
-// function accesslevel() {
+function accessLevel() {
+    var existCookie = "0";
+    var allCookie = decodeURIComponent(document.cookie);
+    var splitedCookie = allCookie.split(";");
+    for (var i = 0; i < splitedCookie.length; i++) {
+        if (splitedCookie[i].indexOf("token=") == 0) {
+            existCookie = splitedCookie[i].substring(6, splitedCookie[i].length);
+        }
+    }
 
-// }
+    fetch("nss.php?do=getAccessLevel&token=" + existCookie)
+        .then(res => {
+            return res.json();
+        })
+        .then(feedback => {
+            document.getElementById("showUsername").innerHTML = feedback.username;
+            document.getElementById('toLoginPage').style.visibility = 'hidden';
+            if (feedback.accessLevel == 0) {
+                document.getElementById('toLoginPage').style.visibility = 'visible';
+            }
+        });
+}
 
 // function list_judgers() {
 
