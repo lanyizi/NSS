e8a0c05f9eb3df40adb8c77197840e3cd06cb58f
RC <richcfno1@users.noreply.github.com>
2019-08-04 23:32:22 -0700

    Test4

--- nss.js
+++ nss.js
@@ -1,36 +1,35 @@
-alert("TEST");
+function login() {
+    alert("LoginTest")
+        // var transdata = { 'username': username, 'password': password };
 
-//function login() {
-// var transdata = document.getElementById(login_info);
+    // //temp is a temporary value of feedback
+    // var temp = fetch("/nss.php?do=login", {
+    //         method: 'post',
+    //         body: JSON.stringify(transdata),
+    //         headers: { 'Content-Type': 'application/json' }
+    //     })
+    //     .then(res => res.json());
 
-// //temp is a temporary value of feedback
-// var temp = fetch("/nss.php?do=login", {
-//         method: 'post',
-//         body: JSON.stringify(transdata),
-//         headers: { 'Content-Type': 'application/json' }
-//     })
-//     .then(res => res.json());
+    // //If not pass
+    // if (temp = '0') {
+    //     alert("Wrong username or password")
+    //         //window.location.href = "login_page.html";
+    //     return "Login Fail";
+    // }
 
-// //If not pass
-// if (temp = '0') {
-//     alert("Wrong username or password")
-//         //window.location.href = "login_page.html";
-//     return "Login Fail";
-// }
+    // //Get the time and set for 2 hours access
+    // var date = new Date();
+    // date.setTime(date.getTime() + (2 * 60 * 60 * 1000));
 
-// //Get the time and set for 2 hours access
-// var date = new Date();
-// date.setTime(date.getTime() + (2 * 60 * 60 * 1000));
+    // //Get token
+    // var feedback = JSON.parse(temp);
+    // var token = feedback[Object.keys(feedback)[1]];
 
-// //Get token
-// var feedback = JSON.parse(temp);
-// var token = feedback[Object.keys(feedback)[1]];
+    // //Set cookie
+    // document.cookie = "Token=" + token + "; expires=" + date.toGMTString();
 
-// //Set cookie
-// document.cookie = "Token=" + token + "; expires=" + date.toGMTString();
-
-// return "Login Success";
-//}
+    // return "Login Success";
+}
 
 // function accesslevel() {
 
