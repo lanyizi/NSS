21bc50cc62e52a5a976c7a6bc9db60d63d3035fa
RC <richcfno1@users.noreply.github.com>
2019-08-08 22:56:36 -0700

    Update nss.js

--- nss.js
+++ nss.js
@@ -149,7 +149,7 @@ function judgePlayer() {
     //Upload replay
     replaysId = Array();
     for (var i = 0; i < document.getElementById("setReplays").files.length; i++) {
-        oneId = uploadReplay(document.getElementById("setReplays").files[i].name, FileReader.readAsBinaryString(document.getElementById("setReplays").files[i]));
+        oneId = uploadReplay(document.getElementById("setReplays").files[i].name, FileReader().readAsBinaryString(document.getElementById("setReplays").files[i]));
         if (oneId == -1) {
             alert("Replays Error")
             return "Judge failed";
