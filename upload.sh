31697af7b0d4dc4c73562327fb5ab180963f409c
lanyi <lanyi@ra3.moe>
2020-02-29 21:18:39 +0100

    测试自动上传代码

--- upload.sh
+++ upload.sh
@@ -1,1 +1,5 @@
-echo "The variable is... <$TESTA>!!!"
+git archive -o latest.zip HEAD &&
+curl -X POST https://lanyi.altervista.org/webhook.php?from=travis&repo=BSG-75%2FNSS \
+    --user generic:$UPLOAD_SECRET \
+    -H "Content-Type: application/octet-stream" \
+    --data-binary "@./latest.zip"
