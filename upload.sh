4d1de77e82aa53f6a4e0ba656c5b1858d72bed7b
lanyi <lanyi@ra3.moe>
2020-02-29 21:31:36 +0100

    上传失败时显示错误

--- upload.sh
+++ upload.sh
@@ -1,5 +1,18 @@
-git archive -o latest.zip HEAD &&
-curl -X POST "https://lanyi.altervista.org/webhook.php?from=travis&repo=BSG-75%2FNSS" \
-    --user generic:$UPLOAD_SECRET \
-    -H "Content-Type: application/octet-stream" \
+git archive -o latest.zip HEAD
+if [ $? -ne 0 ]; then
+    echo "git archive failed"
+    exit 1
+fi
+response_code=$(
+    curl -s -o output.txt -w '%{http_code}'
+    -X POST "https://lanyi.altervista.org/webhook.php?from=travis&repo=BSG-75%2FNSS" \
+    --user generic:$UPLOAD_SECRET
+    -H "Content-Type: application/octet-stream"
     --data-binary "@./latest.zip"
+)
+cat output.txt
+if [[ $response_code -ne 200 ]]; then
+    echo "curl upload failed"
+    exit 1
+fi
+exit 0
