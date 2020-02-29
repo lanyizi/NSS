c18dd7b270263d22fdde04f8f4029039b63e385f
lanyi <lanyi@ra3.moe>
2020-02-29 21:20:05 +0100

    修

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git archive -o latest.zip HEAD &&
-curl -X POST https://lanyi.altervista.org/webhook.php?from=travis&repo=BSG-75%2FNSS \
+curl -X POST "https://lanyi.altervista.org/webhook.php?from=travis&repo=BSG-75%2FNSS" \
     --user generic:$UPLOAD_SECRET \
     -H "Content-Type: application/octet-stream" \
     --data-binary "@./latest.zip"
